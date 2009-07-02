7269ccbae6b428003cbbab0088bc16f414e0c26b
Joas Schilling <nickvergessen@gmx.de>
2009-07-02 10:07:33 +0000

    Update + MVC

---
+++ root/adm/mods/nv_newspage_version.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*
+* @package acp
+* @version $Id$
+* @copyright (c) nickvergessen ( http://www.flying-bits.org/ )
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @package nv_altt
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class nv_newspage_version
+{
+	function version()
+	{
+		return array(
+			'author'	=> 'nickvergessen',
+			'title'		=> 'NV Newspage',
+			'tag'		=> 'nv_newspage',
+			'version'	=> '1.0.1',
+			'file'		=> array('www.flying-bits.org', 'updatecheck', 'nv_newspage.xml'),
+		);
+	}
+}
+
+?>
