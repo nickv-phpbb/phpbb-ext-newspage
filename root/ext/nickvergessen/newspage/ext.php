285366e013e7e84192b9bed05c64667a9ad308bc
Derky <derky@phpbb.com>
2013-07-13 13:08:32 -0400

    Add missing ext.php

---
+++ root/ext/nickvergessen/newspage/ext.php
@@ -0,0 +1,9 @@
+<?php
+
+// this file is not really needed, when empty it can be ommitted
+// however you can override the default methods and add custom
+// installation logic
+
+class phpbb_ext_nickvergessen_newspage_ext extends phpbb_extension_base
+{
+}
