98c26bf0053fea9b0c17e976a851aafe01c9ff23
Joas Schilling <nickvergessen@gmx.de>
2014-04-11 00:42:25 +0200

    [develop-ascraeus] Use DB Driver Interface for type hinting

--- newspage.php
+++ newspage.php
@@ -38,7 +38,7 @@ class newspage
 	* @param \phpbb\auth\auth		$auth		Auth object
 	* @param \phpbb\cache\service	$cache		Cache object
 	* @param \phpbb\config\config	$config		Config object
-	* @param \phpbb\db\driver\driver	$db		Database object
+	* @param \phpbb\db\driver\driver_interface	$db		Database object
 	* @param \phpbb\request\request		$request	Request object
 	* @param \phpbb\template\template	$template	Template object
 	* @param \phpbb\user		$user		User object
@@ -49,7 +49,7 @@ class newspage
 	* @param string			$root_path	phpBB root path
 	* @param string			$php_ext	phpEx
 	*/
-	public function __construct(\phpbb\auth\auth $auth, \phpbb\cache\service $cache, \phpbb\config\config $config, \phpbb\db\driver\driver $db, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user, \phpbb\content_visibility $content_visibility, \phpbb\controller\helper $helper, \nickvergessen\newspage\helper $news_helper, \phpbb\pagination $pagination, $root_path, $php_ext)
+	public function __construct(\phpbb\auth\auth $auth, \phpbb\cache\service $cache, \phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user, \phpbb\content_visibility $content_visibility, \phpbb\controller\helper $helper, \nickvergessen\newspage\helper $news_helper, \phpbb\pagination $pagination, $root_path, $php_ext)
 	{
 		$this->auth = $auth;
 		$this->cache = $cache;
